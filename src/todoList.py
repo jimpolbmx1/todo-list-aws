2434b44e32cb143f73c7567f46732dc0b9ecf006
jimpolbmx1 <jpdavalos1@hotmail.com>
2022-05-25 02:45:21 +0000

    Adding translate test

--- src/todoList.py
+++ src/todoList.py
@@ -6,6 +6,8 @@ import json
 import functools
 from botocore.exceptions import ClientError
 
+translate = boto3.client(service_name='translate',
+                         region_name='us-east-1', use_ssl=True)
 
 def get_table(dynamodb=None):
     if not dynamodb:
@@ -39,8 +41,6 @@ def get_item(key, dynamodb=None):
 
 def get_translate(key, lg, dynamodb=None):
     table = get_table(dynamodb)
-    translate = boto3.client(service_name='translate',
-                             region_name='us-east-1', use_ssl=True)
     result = table.get_item(
         Key={
             'id': key
