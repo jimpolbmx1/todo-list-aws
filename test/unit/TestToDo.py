6087dabd15420b9f50c41e97ec2cdf0836e1692b
jimpolbmx1 <jpdavalos1@hotmail.com>
2022-06-22 22:37:00 +0000

    Test errors

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -231,8 +231,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.table = table = Mock()
         table.delete_item.side_effect = Exception('Boto3 Exception')
         with self.assertRaises(Exception) as exc:
-            result=delete_item(self.dynamodb)
-            delete_item(self.dynamodb)
+            result = delete_item(exc,self.dynamodb)
             self.assertTrue('Boto3 Exception' in exc.exception)
         print (result)
         print ('End: test_delete_todo_error')
